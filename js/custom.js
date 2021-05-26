6dff059289197980aca5cdc1cae75caf22d48ddb
Naseem Ahmad <naseem@neurosensum.com>
2021-05-26 13:56:31 +0530

    home page design

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-      $('.carousel').carousel({
+
+      // home page carousal
+      $('.carousel.home-page-carousal').carousel({
             interval: 3000
       });
+      
+
+      // home page class add on hover dryer section
+      $('.dryer-model-right-main').hover(function(){
+            $('.dryer-model-left-main').addClass('active');
+      }, function(){
+            $('.dryer-model-left-main').removeClass('active');
+      });
+
+
 });
+
