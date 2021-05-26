023243714d5ab7ebbe87833b6b69a2ef6dda2820
Naseem Ahmad <naseem@neurosensum.com>
2021-05-26 17:08:59 +0530

    home page design

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,20 @@ $(document).ready(function(){
             $('.dryer-model-left-main').addClass('active');
       }, function(){
             $('.dryer-model-left-main').removeClass('active');
+      });  
+
+
+      // custom sticky navbar with fixed on scroll
+      $(window).scroll(function() {
+
+            var windowTop = $(window).scrollTop();
+        
+            if (windowTop > 90) {
+              $('.custom-sticky').addClass('fixed-menu');
+            } else {
+              $('.custom-sticky').removeClass('fixed-menu');
+            }
+
       });
-
-
 });
 
